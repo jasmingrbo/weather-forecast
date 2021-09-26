4c1b75c94c29ec48a0d01de9896a0e7107798f31
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2021-09-26 17:43:44 +0200

    Update dependencies, resolve warnings and refactor code

--- app/src/main/java/ba/grbo/weatherforecast/ui/theme/Color.kt
+++ app/src/main/java/ba/grbo/weatherforecast/ui/theme/Color.kt
@@ -1,8 +1,22 @@
 package ba.grbo.weatherforecast.ui.theme
 
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+private val Purple200 = Color(0xFFBB86FC)
+private val Purple500 = Color(0xFF6200EE)
+private val Purple700 = Color(0xFF3700B3)
+private val Teal200 = Color(0xFF03DAC5)
+
+val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
